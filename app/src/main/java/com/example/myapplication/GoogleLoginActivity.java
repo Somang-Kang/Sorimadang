b60fa118186793d58c94473ab54c4ed0e47b5807
손효민 <sondo100@hanmail.net>
2021-08-03 23:35:41 +0900

    buttonbefore

--- app/src/main/java/com/example/myapplication/GoogleLoginActivity.java
+++ app/src/main/java/com/example/myapplication/GoogleLoginActivity.java
@@ -4,11 +4,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class GoogleLoginActivity extends AppCompatActivity {
+
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_login);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        //사용자의 ID,기본 프로필 정보를 요청하기위해 객체생성
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        //gso객체를 인자로 줘서 생성.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        /*
+        기존의 사용자가 있는 경우
+        onStart()메소드 위에 코드 추가, 기존에 로그인된 사용자 객체 얻을 수 있음.
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+        * */
+
+
+
     }
 }
